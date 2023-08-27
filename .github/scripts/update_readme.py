8db614cda46be11154c6edcacc463859ffb83e75
PapaPeskwo <tomi.orlovac@gmail.com>
2023-08-27 18:45:48 +0200

    malding

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -16,52 +16,42 @@ regex = r"\|\s*\[([^\]]+)\]\(https://github.com/([^\)]+)\)\s*\|\s*([^|]+)\s*\|\s
 with open("README.md", "r") as file:
     content = file.read()
 
-# Extract the projects section from the README
 projects_start = content.index("## Projects")
-
-matches = list(re.finditer(regex, content))
-
-if not matches:
-    print("No project entries found.")
-    projects_end = content.index("## Certificates")  # or wherever your projects section typically ends
-else:
-    projects_end = max([m.end() for m in matches])
-
-
+projects_end = content.index("## Certificates") if "## Certificates" in content else None
 projects_section = content[projects_start:projects_end]
 
 # Collect all repository details in a list
 repos = []
 
-for match in re.finditer(regex, projects_section):
+matches = list(re.finditer(regex, projects_section))
+print(f"Found {len(matches)} repositories in the projects section.")
+
+for match in matches:
     repo_url, repo_fullname, description, _ = match.groups()
     repo_api_url = f"https://api.github.com/repos/{repo_fullname}/commits/master"
-    response = requests.get(repo_api_url, headers=headers)
-    response.raise_for_status()
-    last_updated = response.json()["commit"]["committer"]["date"]
-    formatted_date = "/".join(reversed(last_updated.split("T")[0].split("-")))
+    try:
+        response = requests.get(repo_api_url, headers=headers)
+        response.raise_for_status()
+        last_updated = response.json()["commit"]["committer"]["date"]
+        formatted_date = "/".join(reversed(last_updated.split("T")[0].split("-")))
+        repos.append({
+            "repo_url": repo_url,
+            "repo_fullname": repo_fullname,
+            "description": description,
+            "formatted_date": formatted_date,
+            "last_updated": last_updated
+        })
+    except Exception as e:
+        print(f"Failed to get last updated date for {repo_fullname}. Error: {e}")
 
-    repos.append({
-        "repo_url": repo_url,
-        "repo_fullname": repo_fullname,
-        "description": description,
-        "formatted_date": formatted_date,
-        "last_updated": last_updated
-    })
-
-# Sort the repositories based on the last_updated time
 sorted_repos = sorted(repos, key=lambda x: x["last_updated"], reverse=True)
-
-# Construct the new projects section
 new_projects = "## Projects\n| Project Name | Project Description | Last Updated: |\n| --- | --- | --- |\n"
 for repo in sorted_repos:
-    new_line = f"| [{repo['repo_url']}](https://github.com/{repo['repo_fullname']}) | {repo['description']} | {repo['formatted_date']} |\n"
+    new_line = f"| [{repo['repo_fullname'].split('/')[-1]}](/{repo['repo_fullname']}) | {repo['description']} | {repo['formatted_date']} |\n"
     new_projects += new_line
 
-# Replace the old projects section with the new one
-new_content = content[:projects_start] + new_projects + content[projects_end:]
+new_content = content.replace(projects_section, new_projects)
 
-# Overwrite README if there are changes
 if new_content != content:
     with open("README.md", "w") as file:
         file.write(new_content)
